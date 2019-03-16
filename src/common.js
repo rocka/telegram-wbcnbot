c1ffd902511080dda1fb3d5394da8d65bec7f2a0
Rocka <i@rocka.me>
2019-03-16 21:03:31 +0800

    refactor: clean up getTweetStatus

--- src/common.js
+++ src/common.js
@@ -64,37 +64,68 @@ function runScripts(scripts) {
     };
 }
 
-function makeLocals(url, { data, config, performance }) {
-    const info = data.status.page_info || {};
-    let locals = {
-        url,
-        id: data.status.id,
-        type: info.type || 'text',
-        title: `${data.status.user.screen_name} 的微博`,
-        pub_time: data.status.created_at,
-        username: data.status.user.screen_name,
-        content: Cheerio.load(data.status.text).text()
+function getTweetStatus(status) {
+    let text, html;
+    if (status.isLongText) {
+        text = status.longText.longTextContent;
+        html = status.longText.longTextContent;
+        status.longText.url_objects.forEach(o => {
+            text = text.replace(o.url_ori, o.info.url_long);
+            html = html.replace(o.url_ori, `<a href="${o.info.url_long}">${o.info.url_long}</a>`);
+        });
+        html = html.replace(/\n/g, '<br>');
+    } else {
+        const $ = Cheerio.load(status.text);
+        text = $('body').text();
+        $('span.url-icon').each((i, e) => {
+            const s = $(e);
+            s.replaceWith(s.children().attr('alt'));
+        });
+        $('a').each((i, e) => {
+            const a = $(e);
+            if (a.text() === '网页链接') {
+                a.replaceWith(`<a href="${a.attr('href')}">${a.attr('href')}</a>`);
+            } else if (a.text() === '查看图片') {
+                a.replaceWith(`<img src="${a.attr('href')}"/>`);
+            }
+        });
+        html = $('body').html();
+    }
+    let type = status.page_info ? status.page_info.type : 'text';
+    if (status.pic_ids && status.pic_ids.length > 0) type = 'picture';
+    const result = {
+        id: status.id,
+        mid: status.mid,
+        type,
+        user: {
+            id: status.user.id,
+            name: status.user.screen_name,
+            avatar: status.user.profile_image_url
+        },
+        time: status.created_at,
+        content: { html, text }
     };
     // TODO: support type: search_topic ?
-    // TODO: handle retweets
-    if (data.status.pics) locals.type = 'picture';
-    switch (locals.type) {
+    switch (type) {
         case 'picture':
-            locals.picture = data.status.pics.map(p => p.large.url);
-            locals.picture_thumb = data.status.pics.map(p => p.url);
+            result.picture = (status.pics || []).map(p => p.large.url);
             break;
         case 'video':
-            locals.video = [
-                info.media_info.stream_url,
-                // info.media_info.stream_url_hd
-            ];
-            locals.thumbnail = [info.page_pic.url];
+            result.video = [status.page_info.media_info.stream_url];
+            result.thumbnail = [status.page_info.page_pic.url];
             break;
         case 'article':
-            locals.cover = [info.page_pic.url];
+            result.cover = [status.page_info.page_pic.url];
             break;
-        default:
-            break;
+    }
+    return result;
+}
+
+function makeLocals(url, { data, config, performance }) {
+    let locals = getTweetStatus(data.status);
+    locals.url = url;
+    if (data.status.retweeted_status) {
+        locals.retweet = getTweetStatus(data.status.retweeted_status);
     }
     return locals;
 }
