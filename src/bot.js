297f4811b09e4e102f3e0a1006054092eb9cf759
Rocka <i@rocka.me>
2020-04-24 17:08:28 +0800

    feat: show some tips if inline results unavailable

--- src/bot.js
+++ src/bot.js
@@ -12,22 +12,31 @@ const rhash = process.env.IV_RHASH;
 
 /**
  * @param {WBCNBot.WeiboLocals} locals
+ * @returns {string}
  */
-function makeTextMsg(locals) {
+function makeTextMessageContent(locals) {
     if (!locals.id) {
-        return [];
+        return '';
     }
     let iv = `${process.env.DOMAIN}/p/${locals.id}`;
     if (rhash) {
         iv = `https://t.me/iv?${qs.stringify({ rhash, url: iv })}`
     }
     const orig = `https://m.weibo.cn/detail/${locals.mid}`;
+    return Pug.renderFile('./templates/message_text.pug', { iv, orig });
+}
+
+/**
+ * @param {WBCNBot.WeiboLocals} locals
+ * @returns {import('telegraf/typings/telegram-types').InlineQueryResult[]}
+ */
+function makeInlineQueryResult(locals) {
     return [{
         type: 'article',
         id: `${locals.id}_a`,
         title: `${locals.user.name} 的微博`,
         input_message_content: {
-            message_text: Pug.renderFile('./templates/message_text.pug', { iv, orig }),
+            message_text: makeTextMessageContent(locals),
             parse_mode: 'html'
         },
         url: locals.url,
@@ -56,22 +65,38 @@ function makeResults(locals) {
     }
 }
 
-bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
-    if (inlineQuery.query.length < 5) {
-        console.log('[Bot Query] Skipped empty query.');
-        return answerInlineQuery([]);
+bot.on('inline_query', async ctx => {
+    const { id, query } = ctx.inlineQuery;
+    if (query.length === 0) {
+        console.log('[Inline Query] empty query');
+        return ctx.answerInlineQuery([], {
+            switch_pm_text: 'All Systems Operational :)',
+            switch_pm_parameter: `placeholder_empty_${id}`
+        });
+    } else if (query.length < 5) {
+        console.log('[Inline Query] short query');
+        return ctx.answerInlineQuery([], {
+            switch_pm_text: 'Input too short ...',
+            switch_pm_parameter: `placeholder_short_${id}`
+        });
     }
-    console.log('[Bot Query]', inlineQuery.query);
-    const url = await Common.getWeiboURL(inlineQuery.query);
-    console.log('url:', url);
+    console.log('[Inline Query]', query);
+    const url = await Common.getWeiboURL(query);
+    console.log('[Inline Query] Weibo URL:', url);
     const locals = await Common.getLocals(url);
-    console.log('locals: ', locals);
-    // const results = makeResults(locals);
-    return answerInlineQuery(makeTextMsg(locals));
-})
+    console.log('[Inline Query] Locals:', locals);
+    if (!Object.prototype.hasOwnProperty.call(locals, 'id')) {
+        console.log('[Inline Query] invalid locals');
+        return ctx.answerInlineQuery([], {
+            switch_pm_text: 'Weibo not available :(',
+            switch_pm_parameter: `placeholder_invalid_${id}`
+        });
+    }
+    return ctx.answerInlineQuery(makeInlineQueryResult(locals));
+});
 
 bot.catch((err) => {
-    console.log('[ERROR]', err);
+    console.log('[Bot ERROR]', err);
 });
 
 module.exports = bot;
